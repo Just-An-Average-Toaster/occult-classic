a372a1eb617d1f8d29e1cfb11d27e48ea8b710b5
Just-An-Average-Toaster <114497348+Just-An-Average-Toaster@users.noreply.github.com>
2023-02-03 12:04:05 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -34,8 +34,17 @@ function moveBattleMenuSelection (direction: number) {
     cursor.right = selectedMenuButton.left
     cursor.y = selectedMenuButton.y
 }
+function Spawn_enemies () {
+    for (let value of tiles.getTilesByType(assets.tile`myTile6`)) {
+        CatMap = sprites.create(assets.image`myImage1`, SpriteKind.Enemy)
+        tiles.placeOnTile(CatMap, value)
+        tiles.setTileAt(value, assets.tile`myTile0`)
+    }
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    moveBattleMenuSelection(0)
+    if (Inbattle == 1) {
+        moveBattleMenuSelection(0)
+    }
 })
 function createMenuButtonSprite (text: string) {
     newMenuButton = textsprite.create(text, 12, 1)
@@ -77,7 +86,9 @@ function showOrHideWeapmon (theWeapmon: Sprite, shouldHide: boolean) {
     statusbars.getStatusBarAttachedTo(StatusBarKind.Health, theWeapmon).setFlag(SpriteFlag.Invisible, shouldHide)
 }
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    moveBattleMenuSelection(3)
+    if (Inbattle == 1) {
+        moveBattleMenuSelection(3)
+    }
 })
 function battleDodge () {
 	
@@ -119,7 +130,9 @@ function createBattleMenu () {
     cursor.y = selectedMenuButton.y
 }
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    moveBattleMenuSelection(1)
+    if (Inbattle == 1) {
+        moveBattleMenuSelection(1)
+    }
 })
 function moveWeapmon (theWeapmon: Sprite, x: number, y: number) {
     theWeapmon.setPosition(x, y)
@@ -127,7 +140,9 @@ function moveWeapmon (theWeapmon: Sprite, x: number, y: number) {
     sprites.readDataSprite(theWeapmon, "label").left = weapons.left - 9
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    moveBattleMenuSelection(2)
+    if (Inbattle == 1) {
+        moveBattleMenuSelection(2)
+    }
 })
 function battleBlock () {
 	
@@ -177,6 +192,9 @@ function battleFight (weapmon: Sprite, enemy: Sprite) {
 }
 function BattleTime () {
     scene.setBackgroundImage(assets.image`myImage`)
+    scene.centerCameraAt(0, 0)
+    tiles.setCurrentTilemap(tilemap`level6`)
+    Inbattle = 1
     Tomothymon = createWeapmon(assets.image`myImage0`, "Tomothy", 20, 4)
     moveWeapmon(Tomothymon, 30, 20)
     Catmon = createWeapmon(assets.image`myImage1`, "Cat", 20, 3)
@@ -189,6 +207,27 @@ function BattleTime () {
 function battleItems () {
 	
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
+        if (Tomothy_Map.x < CatMap.x) {
+            Tomothy_Map.x += -5
+            Warningmeter += 1
+        } else if (Tomothy_Map.x > CatMap.x) {
+            Tomothy_Map.x += 5
+            Warningmeter += 1
+        }
+        if (Angermeter == 0 && Warningmeter == 10) {
+            story.spriteSayText(value, "Hey man! Quit bumping into me!", 12)
+            Warningmeter = 0
+            Angermeter = 1
+        } else if (Angermeter == 1 && Warningmeter == 10) {
+            story.spriteSayText(value, "Thats it! Bring it on!", 12)
+            BattleTime()
+        }
+    }
+})
+let Angermeter = 0
+let Warningmeter = 0
 let Bat: Sprite = null
 let Catmon: Sprite = null
 let Tomothymon: Sprite = null
@@ -200,12 +239,17 @@ let textSprite: TextSprite = null
 let statusbar: StatusBarSprite = null
 let weapons: Sprite = null
 let newMenuButton: TextSprite = null
+let Inbattle = 0
+let CatMap: Sprite = null
 let cursor: Sprite = null
 let blockMenuButton: TextSprite = null
 let fightMenuButton: TextSprite = null
 let itemsMenuButton: TextSprite = null
 let dodgeMenuButton: TextSprite = null
 let selectedMenuButton: TextSprite = null
+let Tomothy_Map: Sprite = null
 tiles.setCurrentTilemap(tilemap`temp map`)
-let Tomothy_Map = sprites.create(assets.image`myImage0`, SpriteKind.Player)
+Spawn_enemies()
+Tomothy_Map = sprites.create(assets.image`myImage0`, SpriteKind.Player)
 controller.moveSprite(Tomothy_Map)
+scene.cameraFollowSprite(Tomothy_Map)
